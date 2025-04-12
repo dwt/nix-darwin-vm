8cabec0d38b2ae706590200eb8103d0242c23606
‮rekcäH nitraM‮ <spamfaenger@gmx.de>
2025-04-12 16:05:47 +0200

    Better naming

--- modules/vm.nix
+++ modules/vm.nix
@@ -5,11 +5,12 @@
   ...
 }:
 let
-  darwinArch = lib.replaceStrings [ "linux" ] [ "darwin" ] pkgs.system;
-  darwinPkgs = inputs.nixpkgs.legacyPackages.${darwinArch};
+  matchingDarwinArch = lib.replaceStrings [ "linux" ] [ "darwin" ] pkgs.system;
+  darwinPkgs = inputs.nixpkgs.legacyPackages.${matchingDarwinArch};
 in
 {
   # Make VM output to the terminal instead of a separate window
   virtualisation.vmVariant.virtualisation.graphics = false;
+  # Allow the VM to start on darwin
   virtualisation.vmVariant.virtualisation.host.pkgs = darwinPkgs;
 }
