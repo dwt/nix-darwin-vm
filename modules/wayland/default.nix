485565ad73c8e677bb7c53ead147694e3cf43cc2
‮rekcäH nitraM‮ <spamfaenger@gmx.de>
2025-04-15 22:50:31 +0200

    german keyboard setup

--- modules/wayland/default.nix
+++ modules/wayland/default.nix
@@ -62,10 +62,19 @@
   #  [[ "$(tty)" == /dev/tty1 ]] && ${nixpkgs.lib.getBin weston}/bin/weston
   #'';
 
-  system.userActivationScripts.setupDesktop = ''
-    mkdir -p ~/.config/labwc
-    cp --symbolic-link --update ${./menu.xml} ~/.config/labwc/menu.xml
-    cp --symbolic-link --update ${./autostart} ~/.config/labwc/autostart
-    echo '${if withNaturalKeyboard then "XKB_DEFAULT_LAYOUT=de" else ""}' > ~/.config/labwc/environment
-  '';
+  system.userActivationScripts.setupDesktop =
+    ''
+      set -x
+      mkdir -p ~/.config/labwc
+      cp --symbolic-link --update ${./menu.xml} ~/.config/labwc/menu.xml
+      cp --symbolic-link --update ${./autostart} ~/.config/labwc/autostart
+    ''
+    + lib.optionalString (withNaturalKeyboard) ''
+      {
+        echo XKB_DEFAULT_LAYOUT="de"
+        echo XKB_DEFAULT_VARIANT="mac"
+        echo XKB_DEFAULT_MODEL="apple_laptop"
+        echo XKB_DEFAULT_OPTIONS="lv3:ralt_switch,lv3:lalt_switch,apple:badmap"
+      } > ~/.config/labwc/environment
+    '';
 }
